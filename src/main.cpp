6a83921d022f1a641fab65f638ead130b0acc761
Sam Nicknam <s.nicknam@gmail.com>
2019-06-18 12:56:53 +0200

    Vehicle read and notifier vms error.
    Also added linker error fix.

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,12 @@ using namespace Positioning;
 using namespace Notifying;
 using namespace Messaging;
 
+#define USB_SERIAL_BAUD 9600
+
 #define VEHICLE_SERIAL &Serial3
 #define MAX_CHARGING_CURRENT 6
 #define MAX_REVERSE_CHARGING_CURRENT 6
+#define VEHICLE_TIMEOUT_THRESHOLD 3000
 
 #define LED_PIN_RED 23
 #define LED_PIN_GREEN 22
@@ -28,8 +31,14 @@ std::shared_ptr<LocationProvider> location;
 std::shared_ptr<Notifier> notifier;
 std::shared_ptr<JsonHandler> messageHandler;
 
+uint32_t prevVehicleMillis = 0;
+
 void setup()
 {
+	// Prevent linking error.
+	Serial.begin(USB_SERIAL_BAUD);
+	Serial.println(F("Started"));
+
 	// TODO - implement setup
 	vehicle = std::make_shared<VehicleClient>(
 		std::move(std::make_unique<AcPropulsion>(
@@ -42,4 +51,14 @@ void setup()
 void loop()
 {
 	// TODO - implement loop
+	VehicleData vehicleData = vehicle->getData();
+	if (VehicleData::isValid(vehicleData.timestamp))
+	{
+		prevVehicleMillis = millis();
+		notifier->off();
+	}
+	else if (millis() - prevVehicleMillis > VEHICLE_TIMEOUT_THRESHOLD)
+	{
+		notifier->setVmsError();
+	}
 }
