401e56878acd14a90c8599f20ac06c7627214eb9
Thomas Brouwers <tmbrouwers@gmail.com>
2019-06-19 16:04:42 +0200

    Implemented gps parser (#15)
    * added parser and serial read
    
    * fixed parser and added simple validator
    
    * removed changes
    
    * added parser and serial read
    
    * fixed parser and added simple validator
    
    * Recomitted deleted files
    
    * implemented change requests
    
    * Delete structure.hash
    
    * implemented new change requests
    
    * Just one teensy little pointer removal.

--- src/Positioning/SamM8Q.cpp
+++ src/Positioning/SamM8Q.cpp
@@ -2,12 +2,62 @@
 
 using namespace Positioning;
 
-unsigned char* SamM8Q::readData()
+SamM8Q::SamM8Q(HardwareSerial *serial)
 {
-	// TODO - implement SamM8Q::readData
+	this->serial = serial;
+	serial->begin(9600);
 }
 
-Location SamM8Q::parseData(unsigned char gpsData[])
+String SamM8Q::readData()
 {
-	// TODO - implement SamM8Q::parseData
+	int iterations = 0;
+	while (serial->available())
+	{
+		const String line = serial->readStringUntil('\n');
+		if (line.startsWith("$GNGGA"))
+		{
+			return line;
+		}
+		else if (iterations == 100)
+		{
+			return "error"; // no line with $GNGGA found
+		}
+		iterations++;
+	}
+	return "error"; // serial not available
+}
+
+Location SamM8Q::parseData(String line)
+{
+	Location loc;
+
+	if (line != "error")
+	{
+		loc.longitude = getValue(line, ',', 4).toFloat();
+		loc.directionLong = getValue(line, ',', 5).charAt(0);
+		loc.latitude = getValue(line, ',', 2).toFloat();
+		loc.directionLat = getValue(line, ',', 3).charAt(0);
+		loc.altitude = getValue(line, ',', 9).toFloat();
+		loc.timestamp = getValue(line, ',', 1).toFloat();
+	}
+	return loc;
+}
+
+String SamM8Q::getValue(String data, char separator, int index)
+{
+	int found = 0;
+	int strIndex[] = {0, -1};
+	int maxIndex = data.length() - 1;
+
+	for (int i = 0; i <= maxIndex && found <= index; i++)
+	{
+		if (data.charAt(i) == separator || i == maxIndex)
+		{
+			found++;
+			strIndex[0] = strIndex[1] + 1;
+			strIndex[1] = (i == maxIndex) ? i + 1 : i;
+		}
+	}
+
+	return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
